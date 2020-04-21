1c405a79b508dcc39a3d58dc9d680e0a2a843be0
littlefxc <12741723+littlefxc@users.noreply.github.com>
2020-04-21 09:35:39 +0800

    fix(user-service):添加异常统一处理器

--- user-service/src/main/java/com/example/UserApplication.java
+++ user-service/src/main/java/com/example/UserApplication.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author fengxuechao
  * @date 2020/3/23
  */
+@Slf4j
 @RestController
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -22,11 +25,18 @@ public class UserApplication {
 
     @GetMapping("users/{username}")
     public User getUser(@PathVariable String username) {
-        return new User(username, 18);
+//        return new User(username, 18);
+        throw new RuntimeException("运行时异常");
     }
 
     @GetMapping("v2/users/{username}")
     public User getUserV2(@PathVariable String username) {
         return new User(username, 18, 0);
     }
+
+    /*@ExceptionHandler(RuntimeException.class)
+    public Object handleRuntimeException(RuntimeException e) {
+        log.error("", e);
+        return ResultBean.error(400, e.getMessage());
+    }*/
 }
